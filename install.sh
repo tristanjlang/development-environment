5a98e78d98081d67535bb9bb5c600d7249f78835
Tristan Lang <tristan@alum.mit.edu>
2016-12-14 10:18:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 chsh -s /usr/local/bin/zsh
 
 # install gvm
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+zsh < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
 gvm install go1.4
 gvm use go1.4
 gvm install go1.6.2
 gvm use go1.6.2
 
 # install nvm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | zsh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install 4.2.6
@@ -24,3 +24,10 @@ echo "set-option -g history-limit 100000" > .tmux.conf
 
 # set up .vimrc
 echo "set number" > .vimrc
+
+# install development dependencies
+brew install autoconf
+brew install automake
+brew install cmake
+brew install libtool
+brew install llvm37
